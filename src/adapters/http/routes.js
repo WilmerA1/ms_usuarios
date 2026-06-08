5cd1c7610c7125bd1eadb70f2a1d8e06899cafc8
BrandonVB09 <b.vallejos.1@estudiantec.cr>
2026-06-07 21:01:29 -0600

    ...

--- src/adapters/http/routes.js
+++ src/adapters/http/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 export function createRoutes(userController) {
   const router = express.Router();
-  //
+  
   router.post('/register', (req, res) => userController.registerUser(req, res));
   router.post('/login', (req, res) => userController.loginUser(req, res));
   router.get('/:id', (req, res) => userController.getUserById(req, res));
